6026c44ac397f07db683d3404a0160d23599a5f4
lzr <125032854@qq.com>
2017-06-15 23:18:48 +0800

    提交

---
+++ helloword/src/main/java/com/lzr/helloword/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lzr.helloword;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView iv = new ImageView(this);
+    }
+}
